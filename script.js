c46cbf0e1775236e2494882daf191e4e7b438a25
Carlota Pearl <email@carlotapearl.com>
2018-05-03 14:34:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,16 +5,22 @@
 // Write a basic Dog() constructor function with the following properties set to 'true'; barks and wagstail.
 // Write your code below:
 
+function Dog() {
+  this.barks = true;
+  this.wagstail = true;
+}
 
 // Create an instance of Dog.
 // Define your variable, fido.
 // Write your code below:
 
+let fido = new Dog();
 
 // Print fido barks. It should print true.
 // It should prin, true.
 // Write your code below:
 
+console.log(fido.barks)
 
 // 2
 // CLASS DECLARATION
@@ -22,16 +28,24 @@
 // It should have the following properties set to 'true'; barks and wagstail.
 // Write your code below:
 
+class DogClass {
+  constructor() {
+    this.barks = true;
+    this.wagstail = true;
+  }
+}
 
 // Create an instance of DogClass()
 // Define your variable, spike
 // Write your code below:
 
+let spike = new DogClass();
 
 // Print spike wagstrail.
 // It should print, true.
 // Write your code below:
 
+console.log(spike.wagstail)
 
 // 3
 // EXTENDS
@@ -39,6 +53,11 @@
 // It should have the property of 'requiresBooks' set to 'true'.
 // Write your code below:
 
+class Subject {
+  constructor() {
+    this.requiresBooks = true;
+  }
+}
 
 // DECLARE A SUB-CLASS MATH
 // Extend from Subject
@@ -46,15 +65,23 @@
 // It should have an additional property of 'requiresCalculator ' set to 'true'.
 // Write your code below:
 
+class Math extends Subject {
+  constructor(requiresBooks) {
+    super(requiresBooks)
+    this.requiresCalculator = true;
+  }
+}
 
 // Declare a 'math' variable, creating an instance of Math
 // Write your code below:
 
+let math = new Math();
 
 //Print math
 // It should print: "Math {requiresBooks: true, requiresCalculator: true}"
 // Write your code below:
 
+console.log(math)
 
 //4
 // SUB-CLASS
@@ -63,14 +90,22 @@
 // It should have an additional property of 'requiresRuler' set to true.
 // Write your code below:
 
+class Geometry extends Math {
+  constructor() {
+    super()
+    this.requiresRuler = true;
+  }
+}
 
 // Declare a 'geometry' variable, creating an instance of Geometry
 // Write your code below:
 
+let geometry = new Geometry();
 
 // Print geometry
 // It should print: "Geometry {requiresBooks: true, requiresCalculator: true, requiresRuler: true}"
 // Write your code below:
+console.log(geometry)
 
 
 //5
@@ -81,15 +116,23 @@
 // Set the value of specialCalculator equals to specialCalculator so you can pass this value when you create an instance of Trigonometry.
 // Write your code below:
 
+class Trigonometry extends Geometry {
+  constructor(specialCalculator) {
+    super()
+    this.specialCalculator = specialCalculator;
+  }
+}
 
 // Declare a 'trigonometry' variable, creating an instance of Trigonometry, passing in an argument of 'Ti-83'.
 // Write your code below:
 
+let trigonometry = new Trigonometry('Ti-83');
 
 // Print trigonometry
 // It should pring: "Trigonometry {requiresBooks: true, requiresCalculator: true, requiresRuler: true, specialCalculator: "Ti-83"}"
 // Write your code below:
 
+console.log(trigonometry)
 
 //6
 // SUB-CLASS
@@ -100,10 +143,21 @@
 // Calculus does not require a ruler, so set that property to 'false'. It does require a special calculator, the 'Ti-89'
 // Write your code below:
 
+class Calculus extends Trigonometry {
+  constructor(requiresRuler, specialCalculator) {
+    super(requiresRuler, specialCalculator)
+    this.requiresRuler = false
+    this.specialCalculator = specialCalculator;
+  }
+}
+
 // Declare a variable of 'calculus', creating an instance of Calculus, passing in a boolean of 'false'
 // Write your code below:
 
+let calculus = new Calculus(false, 'Ti-89')
 
 // Print calculus
 // It should print: "Calculus {requiresBooks: true, requiresCalculator: true, requiresRuler: false, specialCalculator: "Ti-89"}"
 // Write your code below:
+
+console.log(calculus)
